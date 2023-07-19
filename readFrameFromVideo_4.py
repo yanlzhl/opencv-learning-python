96f8720060522c3b11028f8a55a9f73e180ac3c5
yanlzhl <yanlingzhi@jxnu.edu.cn>
2023-07-19 17:52:04 +1000

    cv capture frame form video

--- readFrameFromVideo_4.py
+++ readFrameFromVideo_4.py
@@ -3,13 +3,14 @@ import cv2
 # name and size of window
 cv2.namedWindow("test", cv2.WINDOW_NORMAL);
 
-capture = cv2.VideoCapture("")
+# read frame of video
+capture = cv2.VideoCapture("/Users/neverland/Pictures/202304171621_037425AA.MP4")
 
 while True:
     ret, frame = capture.read() # ret value are true or false.
-    cv2.imshow("front camera image", frame)
+    cv2.imshow("video", frame)
 
-    if(cv2.waitKey(0) == ord('q')):
+    if(cv2.waitKey(40) == ord('q')): # the number of waitKey(40) means delay of time
         break
 
 cv2.destroyWindow("test")
